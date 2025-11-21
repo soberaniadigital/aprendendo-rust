bb4acdae02663079c7c1f2a87173457482c3ddbe
Mikael Menezes da Rocha Barreto <mixaelmenezes@gmail.com>
2025-11-21 10:00:53 -0300

    aprendizado de crates numéricas e de animação 3d em rust

--- integral.rs
+++
@@ -1,7 +0,0 @@
-use integrate;
-fn main(){
-    let yan=integrate::prelude::rectangle_rule(|x| (9.8*65.0/12.5)*(1.0-(-12.5/65.0*x as f64).exp()),0.0,10.0,1000 as usize);
-    let celso=integrate::prelude::newton_rule(|x| (9.8*85.0/12.5)*(1.0-(-12.5/65.0*x as f64).exp()),0.0,10.0,1000 as usize);
-    println!("celso voou {}m em 10seg",celso);
-    println!("yan voou {}m em 10seg",yan);
-}
